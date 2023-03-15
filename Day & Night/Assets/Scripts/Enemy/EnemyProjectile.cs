ef38735ef7cbf8a034704caf69f5d30a08108ac4
Julian <jrg180004@utdallas.edu>
2023-03-15 14:42:25 -0500

    Health implemented
    Collisions aren't working

--- Day & Night/Assets/Scripts/Enemy/EnemyProjectile.cs
+++ Day & Night/Assets/Scripts/Enemy/EnemyProjectile.cs
@@ -3,19 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyProjectile : MonoBehaviour
-{
+public class EnemyProjectile : MonoBehaviour {
     [SerializeField] float _projectileSpeed;
     // [SerializeField] GameObject _particleCollide;
     GameObject player;
+    GameObject enemy;
+    EnemyController stats;
+    PlayerController playerStats;
     
-    void Awake(){
-        player = GameObject.Find("FirstPersonPlayer");
+    void Awake() {
+        player = GameObject.Find("Player");
+        enemy = this.gameObject;
+        stats = enemy.GetComponent<EnemyController>();
+        playerStats = player.GetComponent<PlayerController>();
     }
 
-    void Start(){
-        
-        Destroy(this.gameObject, 15f);
+    void Start() {
+        // Destroy(this.gameObject, 15f);
         transform.LookAt(new Vector3(player.transform.position.x, player.transform.position.y +1,player.transform.position.z));
     }
 
@@ -28,8 +32,8 @@ public class EnemyProjectile : MonoBehaviour
     void OnTriggerEnter(Collider other){
         if(other.tag != "Enemy"){
             if(other.tag == "Player"){
-                //have player take damage
-                // player.PlayerController.TakeDamage(20);
+                Debug.Log("Player takes damage");
+                playerStats.TakeDamage(stats.damage);
             }
             // Instantiate(_particleCollide,transform.position,Quaternion.identity);
             Destroy(this.gameObject);
